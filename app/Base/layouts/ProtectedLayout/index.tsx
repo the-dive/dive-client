07c20aeaa5a42ff7690cfeb4620b6b65c5e004a3
Sameer Rai <sameerai2736@gmail.com>
2022-12-05 14:07:34 +0545

    Add import button in data page

--- app/Base/layouts/ProtectedLayout/index.tsx
+++ app/Base/layouts/ProtectedLayout/index.tsx
@@ -4,10 +4,13 @@ import { Outlet } from 'react-router-dom';
 function ProtectedLayout() {
     return (
         <AppShell
-            padding="md"
+            padding={0}
             header={<Header height={60} p="xs">{/* Header content */}</Header>}
             styles={(theme) => ({
-                main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+                main: {
+                    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+                    display: 'flex',
+                },
             })}
         >
             <Outlet />
